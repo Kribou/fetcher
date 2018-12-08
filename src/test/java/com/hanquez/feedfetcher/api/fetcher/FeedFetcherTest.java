531d03d14ac706d7888b9c9b117aa2e7ae6e36c0
ghanquez <7419028+ghanquez@users.noreply.github.com>
2018-12-08 17:33:42 +0100

    Unit tests: use assertThat (less verbose) assertions

--- src/test/java/com/hanquez/feedfetcher/api/fetcher/FeedFetcherTest.java
+++ src/test/java/com/hanquez/feedfetcher/api/fetcher/FeedFetcherTest.java
@@ -1,6 +1,5 @@
 package com.hanquez.feedfetcher.api.fetcher;
 
-import com.hanquez.feedfetcher.impl.converter.SyndFeedConverter;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -8,7 +7,6 @@ import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import io.github.jsonSnapshot.SnapshotMatcher;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,12 +14,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FeedFetcherTest {
 
     private static final String NIPTECH = "http://feeds.feedburner.com/niptechpodcast";
-    private static final SyndFeedConverter syndFeedConverter = new SyndFeedConverter();
+//    private static final SyndFeedConverter syndFeedConverter = new SyndFeedConverter();
     private static SyndFeed feed;
-    private static SyndFeed convertedFeed;
+//    private static SyndFeed convertedFeed;
 
     @BeforeClass
     public static void beforeAll() throws IOException, FeedException {
@@ -30,7 +30,7 @@ public class FeedFetcherTest {
 
         //Get the feed by FeedFetcher API
         feed = FeedFetcher.getFeed(NIPTECH);
-        convertedFeed = FeedFetcher.getConvertedFeed(NIPTECH, syndFeedConverter);
+//        convertedFeed = FeedFetcher.getConvertedFeed(NIPTECH, syndFeedConverter);
     }
 
     @AfterClass
@@ -39,31 +39,25 @@ public class FeedFetcherTest {
     }
 
     @Test
-    public void getFeedFromRomeTools() {
-        try {
-            URL feedUrl = new URL(NIPTECH);
-            SyndFeedInput input = new SyndFeedInput();
-            SyndFeed feed = input.build(new XmlReader(feedUrl));
-            List<SyndEntry> entries = feed.getEntries();
-            Assert.assertNotNull(entries);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("ERROR: " + ex.getMessage());
-        }
+    public void getFeedFromRomeTools() throws IOException, FeedException {
+        URL feedUrl = new URL(NIPTECH);
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(feedUrl));
+        List<SyndEntry> entries = feed.getEntries();
+        assertThat(entries).isNotNull();
     }
 
     @Test
     public void getFeedEntries() {
         List<SyndEntry> entries = feed.getEntries();
-        Assert.assertNotNull(entries);
-        Assert.assertTrue("No entries found", entries.size() > 0);
+        assertThat(entries).isNotEmpty();
     }
 
     @Test
     public void getDescriptionByFeedFetcherAPI() {
         String description = feed.getDescription();
-        Assert.assertNotNull("No description found", description);
-        Assert.assertTrue("Empty description", !description.isEmpty());
+        assertThat(description).isNotNull();
+        assertThat(description).isNotEmpty();
         SnapshotMatcher.expect(description).toMatchSnapshot();
     }
 
